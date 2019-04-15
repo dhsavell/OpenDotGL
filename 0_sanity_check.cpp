8ce5982be9666b5c3509580e15c3648b377d5dbf
dhsavell <dhsavell05@gmail.com>
2019-04-15 18:31:15 -0400

    Create VS solution and sanity check

---
+++ 0_sanity_check.cpp
@@ -0,0 +1,21 @@
+// 0. Sanity Check
+// See: https://open.gl/context#GLFW
+// 
+// This application simply tries to initialize and then terminate a GLFW context.
+
+#include <GLFW/glfw3.h>
+#include <thread>
+#include <iostream>
+
+int main() {
+	// Initialize our GLFW context. 
+	glfwInit();
+
+	// Do some "work."
+	std::cout << "GLFW initialized! Terminating in 1 second..." << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout << "Terminating GLFW. Goodbye!" << std::endl;
+
+	// Clean up and terminate the context.
+	glfwTerminate();
+}
